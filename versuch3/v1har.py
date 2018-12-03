e2cd1915139bc2f16c851214b1073cb6301c201b
ds-05 <ds-05@in.fhkn.de>
2018-12-03 19:14:36 +0100

    python commit v1

--- versuch3/v1har.py
+++ versuch3/v1har.py
@@ -1,13 +1,49 @@
 from TekTDS2000 import *
 import numpy as np
 import matplotlib.pyplot as plt
-scope = TekTDS2000()
+#scope = TekTDS2000()
 
-#scope.saveCsv(ﬁlename='10kHzk.csv')
-data = np.genfromtxt("10kHzk.csv", delimiter=",")
-dataz = data [:,0:1] 
-data1 = data [:,1:2]
-data2 = data [:,2:3]
+#scope.saveCsv(ﬁlename='harleybentonrichtig.csv')
+data = np.genfromtxt("harleybentonrichtig.csv", delimiter=",")
+datas = data[:,1]
+dataz = data[:,0]
+
+dataz1 = data [1000:1500,0:1]
+dataz2 = data [1750:,0:1]
+
+data1 = data [1000:1500,1:2]
+data2 = data [1750:,1:2]
+
 #plt.plot(dataz,data1)
-plt.plot(dataz,data2)
+#plt.plot(dataz,data1)
+
+#ind = np.argmax(data1)
+#ind2 = np.argmax(data2)
+#periode = dataz2[ind2]-dataz1[ind]
+#gfre = 1/periode
+
+#plt.plot(data)
+sigdauer = 0.01 #10ms
+ablen = len(data) #Signallänge
+abin = sigdauer / ablen #abtastinterval
+abfr = 1 / abin #abtastfrequenz
+
+
+Y = abs(np.fft.fft(datas)) # fft computing and normalization
+Y = Y[range(1250)]
+plt.xlabel('Frequenz($Hertz$)')
+plt.ylabel('Amplitude($Unit$)')
+x = np.linspace(0,abfr/2,1250,endpoint = True)
+
+plt.plot(x,abs(Y))
+plt.show()
+plt.xlabel('Zeit($s$)')
+plt.ylabel('Amplitude($Unit$)')
+plt.plot(dataz,datas)
+
+schw = np.argmax(Y)
+print(np.max(Y))
+print(schw/(ablen * abin))
+
+
 
